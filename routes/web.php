3922267956413cc48edd0fa3fbcc22c1050de387
Alejandro Urbina <urbinagonzalezja@gmail.com>
2022-03-13 20:52:52 +0100

    Created path's

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::post('/test', [UserController::class, 'index']);
