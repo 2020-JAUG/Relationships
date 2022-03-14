4e5a30efdcd275f8e3cb0fb781e46acd76b60dd4
Alejandro Urbina <urbinagonzalezja@gmail.com>
2022-03-15 00:44:35 +0100

    Created factories && imported

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Product::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $title = $this->faker->sentence();
+
+        return [
+            'title' => $title,
+            'slug' => Str::slug($title),
+            'price' => random_int(50, 1000),
+            'quantity' => random_int(0, 5),
+            'views' => random_int(1, 1000),
+            'is_active' => $this->faker->boolean(90),
+            'status' => $this->faker->randomElement(['Draft', 'Published', 'Scheduled']),
+        ];
+    }
+}
